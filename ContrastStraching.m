6b7ff55bec15c1bfff51f3b674c8389d2eee646c
Dhanesh kumar <dhanesh.bcss19@iba-suk.edu.pk>
2022-03-26 15:47:44 +0500

    straching the image

---
+++ ContrastStraching.m
@@ -0,0 +1,8 @@
+%image = imread('.\img\satellite_Image.gif');
+image = imread('beans.jpg');
+stretched_Image = imadjust(image, stretchlim(image, [0.05, 0.95]),[]);
+
+subplot(2,2,1), imshow(image), title('Original Image');
+subplot(2,2,2), imshow(stretched_Image), title('Strethced Image');
+subplot(2,2,3), imhist(image), title('Histogram of Original Image');
+subplot(2,2,4), imhist(stretched_Image), title('Histogram of Stretched Image');
